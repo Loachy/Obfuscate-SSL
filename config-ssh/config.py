98648e5af09d69c00085e311c2c2d262dc58d413
Loachy <ghz.priv@gmail.com>
2015-08-23 00:55:00 +0800

    Update config.py

--- config-ssh/config.py
+++ config-ssh/config.py
@@ -1,1 +1,28 @@
+# user configuration start
 
+_mode = 'server' # 'server' or 'client'
+_password = 'example'
+_hostname = 'myserver.com' # 'Common Name' entered in make_key.pem
+_host = '1.2.3.4' # ip of your server
+
+# user configuration end
+# DO NOT modify any code below
+
+assert _mode in ('server', 'client')
+
+SERVER = dict(
+    run = (_mode == 'server'),
+    listen = ('0.0.0.0', 443),
+    connect = ('127.0.0.1', 22),
+    password = _password.encode(),
+    certfile = 'cert.pem'
+    )
+
+CLIENT = dict(
+    run = (_mode == 'client'),
+    listen = ('127.0.0.1', 2222),
+    connect = (_host, 443),
+    password = _password.encode(),
+    hostname = _hostname,
+    cafile = 'ca.crt'
+    )
