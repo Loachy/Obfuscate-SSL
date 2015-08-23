bddabf8e7ae7554d830cf06a7ae5272b494b72ee
Loachy <ghz.priv@gmail.com>
2015-08-23 16:46:53 +0800

    Update and rename config-ssh/config.py to config-example-socks5/config.py

--- config-example-socks5/config.py
+++ config-example-socks5/config.py
@@ -1,8 +1,8 @@
 # user configuration start
 
 _mode = 'server' # 'server' or 'client'
-_password = 'example'
-_hostname = 'myserver.com' # 'Common Name' entered in make_key.pem
+_password = 'change me!'
+_hostname = 'myserver.com' # 'Common Name' entered in make_key.sh
 _host = '1.2.3.4' # ip of your server
 
 # user configuration end
@@ -13,14 +13,14 @@ assert _mode in ('server', 'client')
 SERVER = dict(
     run = (_mode == 'server'),
     listen = ('0.0.0.0', 443),
-    connect = ('127.0.0.1', 22),
+    connect = ('127.0.0.1', 1080),
     password = _password.encode(),
     certfile = 'cert.pem'
     )
 
 CLIENT = dict(
     run = (_mode == 'client'),
-    listen = ('127.0.0.1', 2222),
+    listen = ('127.0.0.1', 1080),
     connect = (_host, 443),
     password = _password.encode(),
     hostname = _hostname,
